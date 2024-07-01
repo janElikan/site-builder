f6c066f1399c09c4f714068efc0b69a8ff258180
jan Elikan <janelikan@proton.me>
2024-07-01 17:20:31 +0000

    refactor: make read_note a function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use std::fs;
+use std::path::Path;
 
-use color_eyre::eyre::Result;
+use color_eyre::eyre::{OptionExt, Result};
 use gray_matter::{engine::YAML, Matter};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug)]
 struct Note {
+    name: String,
+    meta: NoteMetadata,
+    body: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct NoteMetadata {
     source: Option<String>,
     scope: String,
     r#type: ZettelType,
@@ -23,14 +31,31 @@ enum ZettelType {
 fn main() -> Result<()> {
     color_eyre::install()?;
 
-    let matter = Matter::<YAML>::new();
-
-    let file = fs::read_to_string("/nix/persist/active-externalism/data/website.md")?;
-    let file = matter.parse(&file);
-    let props: Note = file.data.unwrap().deserialize()?;
-    let body = file.content;
-
-    dbg!(&props, body);
+    let note = read_note("/nix/persist/active-externalism/data/website.md")?;
+    dbg!(note);
 
     Ok(())
+}
+
+fn read_note<P: AsRef<Path>>(path: P) -> Result<Note> {
+    let matter = Matter::<YAML>::new();
+
+    let file = fs::read_to_string(&path)?;
+    let file = matter.parse(&file);
+
+    Ok(Note {
+        name: path
+            .as_ref()
+            .iter()
+            .last()
+            .ok_or_eyre("Encountered a file without a name?")?
+            .to_str()
+            .expect("The file should still have a name after type conversion")
+            .to_string(),
+        meta: file
+            .data
+            .ok_or_eyre("The file has no frontmatter")?
+            .deserialize()?,
+        body: file.content,
+    })
 }
